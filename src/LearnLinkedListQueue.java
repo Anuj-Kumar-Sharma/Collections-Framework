938eebca4b02a773deee5ec48bf9cdecaf08d7ca
Safi Badi <safibadi51@gmail.com>
2021-10-29 17:56:42 +0530

    Added .add( ), .remove( ), .element( )

--- src/LearnLinkedListQueue.java
+++ src/LearnLinkedListQueue.java
@@ -5,19 +5,20 @@ public class LearnLinkedListQueue {
 
     public static void main(String[] args) {
 
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> q1 = new LinkedList<>();
 
-        queue.offer(12);
-        queue.offer(24);
-        queue.offer(36);
+        q1.offer(1); // if successfull -> returns true. If unsuccessfull -> returns false
+        q1.offer(2);
+        
+        q1.add(3); // if successfull -> returns true. If unsuccessfull -> throws an exception
+        q1.add(4);
+        System.out.println(q1);
 
-        System.out.println(queue);
+        System.out.println(q1.poll()); // Returns and removes head of queue. Return null if empty
+        System.out.println(q1.remove()); // Returns and removes head of queue. Throws exception if empty
+        System.out.println(q1);
 
-        System.out.println(queue.poll());
-
-        System.out.println(queue);
-
-        System.out.println(queue.peek());
-
+        System.out.println(q1.peek()); // Returns head of queue. Return null if empty
+        System.out.println(q1.element()); // Returns head of queue. Throws exception if empty
     }
 }
